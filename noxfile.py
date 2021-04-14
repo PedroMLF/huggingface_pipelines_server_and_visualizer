1c59480fa4411dd6f024222396c58b4c9c229ed7
PedroMLF <22480495+PedroMLF@users.noreply.github.com>
2021-04-14 23:57:17 +0100

    Add pipelines tests. Add tests to nox

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,17 @@ import nox
 @nox.session
 def lint(session):
     session.install("black")
-    session.run("black", "--check", "-l", "99", ".")
+    session.run("black", "--check", "-l", "99", ".", external=True)
 
 
 @nox.session
 def typing(session):
     session.install("mypy")
     print("\033[1m------> TYPING: Ignore errors related to variable reuse <------\033[0m")
-    session.run("mypy", "--namespace-packages", "-p", "src")
+    session.run("mypy", "--namespace-packages", "-p", "src", external=True)
+
+
+@nox.session
+def test(session):
+    session.install("-r", "requirements.txt")
+    session.run("python", "-m", "pytest", external=True)
