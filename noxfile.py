c03cba09988cc476d10d53723f66f27ec562992d
pedromlf <ferreira.ml.pedro@gmail.com>
2021-04-15 20:58:52 +0100

    Add coverage to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -4,17 +4,18 @@ import nox
 @nox.session
 def lint(session):
     session.install("black")
-    session.run("black", "--check", "-l", "99", ".", external=True)
+    session.run("black", "--check", "-l", "99", ".")
 
 
 @nox.session
 def typing(session):
     session.install("mypy")
     print("\033[1m------> TYPING: Ignore errors related to variable reuse <------\033[0m")
-    session.run("mypy", "--namespace-packages", "-p", "src", external=True)
+    session.run("mypy", "--namespace-packages", "-p", "src")
 
 
 @nox.session
 def test(session):
     session.install("-r", "requirements.txt")
-    session.run("python", "-m", "pytest", external=True)
+    session.run("coverage", "run", "--source=src/", "-m", "pytest")
+    session.run("coverage", "report", "-m")
